fddca4dd528b65e835653cd5938afaed0343e1df
Hossein Soleimani1 <Hossein.Soleimani@SABALIFT.COM>
2025-03-12 09:00:27 +0330

    showQuestion created

--- js/game.js
+++ js/game.js
@@ -1,37 +1,55 @@
-
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 const URL="https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 let formattedData=null;
+let questionIndex=0;
+let correctAnswer=null;
 
 const fetchData = async ()=>{
     const response = await fetch(URL);
     const json= await response.json();
     formattedData=formatData(json.results);
-    console.log(formattedData);
     start();
     }
 
 const start = ()=>{
-        loader.style.display="none";
-        container.style.display="block";
+    showQuestion();
+    loader.style.display="none";
+    container.style.display="block";
         }
-        
-window.addEventListener("load",fetchData);
+
 const formatData=(questionData)=>{
     const result = questionData.map((item)=>{
         const questionObject={question:item.question};
         const answers=[...item.incorrect_answers];
         const correctAnswerIndex=Math.floor(Math.random()*4);
         answers.splice(correctAnswerIndex,0,item.correct_answer);
-        questionObject.answer=answers;
+        questionObject.answers=answers;
         questionObject.correctAnswerIndex=correctAnswerIndex;
-        return questionObject;
+        return questionObject;      
     });
     return result;
 }
 
+const showQuestion = ()=>{
+    const  {question, answers, correctAnswerIndex}=formattedData[questionIndex];
+    correctAnswer=correctAnswerIndex;
+    questionText.innerText=question;
+    answerList.forEach((button,index)=>{
+        button.innerText=answers[index];
+    });
+}
+
+const checkAnswer=()=>{
+
+}
+window.addEventListener("load",fetchData);
+answerList.forEach((button,index)=>{
+    button.addEventListener("click",checkAnswer)
+})
 
 
 
